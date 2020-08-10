9d0f85f19b5767a694d435ec225d20c99a4cc030
Sergey Biryukov <2700036@mail.ru>
2020-08-10 20:51:39 +0300

    Updates

---
+++ precache-manifest.231d6a10790dcaf16f0acc466e48195b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a001762f4831281ad81dc79ca28d5396",
+    "url": "/messenger/index.html"
+  },
+  {
+    "revision": "e4858962d29a2b529af0",
+    "url": "/messenger/static/css/main.31935650.chunk.css"
+  },
+  {
+    "revision": "afe91160c9162063c5df",
+    "url": "/messenger/static/js/2.69fc0e10.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/messenger/static/js/2.69fc0e10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4858962d29a2b529af0",
+    "url": "/messenger/static/js/main.b94ada73.chunk.js"
+  },
+  {
+    "revision": "dbe8fbb5e17e6d4cdb53",
+    "url": "/messenger/static/js/runtime-main.33ac3545.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0de3be803a95d115f327f45c418807e2.js"
+  "/messenger/precache-manifest.231d6a10790dcaf16f0acc466e48195b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/messenger/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
