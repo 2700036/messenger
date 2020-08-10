81b9b0e5c700f6399a35743253935feed8d18ac6
Sergey Biryukov <2700036@mail.ru>
2020-08-10 19:53:55 +0300

    Updates

---
+++ precache-manifest.63513e7abc8e9fbfaefbea6562d3a380.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6292e76e83d87f8f49ec12b34791642b",
+    "url": "/messenger/index.html"
+  },
+  {
+    "revision": "7d2fc3fe6045cc9fe069",
+    "url": "/messenger/static/css/main.a5437bdb.chunk.css"
+  },
+  {
+    "revision": "afe91160c9162063c5df",
+    "url": "/messenger/static/js/2.69fc0e10.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/messenger/static/js/2.69fc0e10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d2fc3fe6045cc9fe069",
+    "url": "/messenger/static/js/main.50340535.chunk.js"
+  },
+  {
+    "revision": "dbe8fbb5e17e6d4cdb53",
+    "url": "/messenger/static/js/runtime-main.33ac3545.js"
+  }
+]);
